06367454e9226c5184db48f9145b735df14fcfb4
notecoding <63888297+notecoding@users.noreply.github.com>
2025-05-28 13:53:27 +0900

    Update ex.sql

--- ex.sql
+++ ex.sql
@@ -1,3 +1,5 @@
+USE myboard;
+
 -- ✅ 사용자 (관리자 + 일반 사용자) pw:admin1234
 INSERT INTO users (name, username, password, is_admin) VALUES
 ('관리자', 'admin', '$2b$10$XkO1eA5Z88YDnHdRTzVN..CqsayGbHjea5p7WvFVU5ESz1yMfQx1K', TRUE);
